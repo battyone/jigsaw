f0cc37e27237a286e4fe42875b0c1fa248de4df6
Gil Shotan <gilsho@cs.stanford.edu>
2018-03-18 01:58:45 -0700

    implement contour smoothing and curvature.

---
+++ src/match.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+Functionality for determining whether two puzzle pieces match.
+
+Usage:
+    match.py show <filepath1> <filepath2>
+"""
+
+import docopt
+import numpy as np
+from matplotlib import pyplot
+
+import piece
+
+
+def _smooth(signal, cutoff, d=1):
+    """Smooth a signal through a high pass filter.
+
+    :param signal: An np.array representing the signal.
+    :param cutoff: The frequency above which all frequencies will be removed.
+    :param d:
+        The sampling rate used to acquire the signal. Makes the cutoff
+        parameter meaningful.
+    :return:
+        The original signal with the high frequencies removed.
+    """
+    f = np.fft.rfft(signal)
+    fx = np.fft.rfftfreq(n=signal.size, d=d)
+    f[fx > cutoff] = 0
+    return np.fft.irfft(f)
+
+
+def _smooth_contour(c, cutoff=2.5e-3):
+    c1 = c[:, 0]
+    c2 = c[:, 1]
+    c1f = _smooth(c1, cutoff)
+    c2f = _smooth(c2, cutoff)
+    return np.vstack([c1f, c2f]).T
+
+
+def _show_match(filepath1, filepath2):
+    p1 = piece.read_piece(filepath1)
+    p2 = piece.read_piece(filepath2)
+    pyplot.subplot(121)
+    pyplot.plot(p1.contour[:, 0], p1.contour[:, 1])
+    pyplot.axis('equal')
+    pyplot.subplot(122)
+    pyplot.plot(p2.contour[:, 0], p2.contour[:, 1])
+    pyplot.axis('equal')
+    pyplot.suptitle('Matching: {} with {}'.format(
+        filepath1, filepath2))
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+    if args['show']:
+        _show_match(args['<filepath1>'], args['<filepath2>'])
--- src/math_utils.py
+++ src/math_utils.py
@@ -16,3 +16,17 @@ def get_rotation_matrix_2d(angle):
 def rotate_2d(points, angle):
     matrix = get_rotation_matrix_2d(angle)
     return np.matmul(matrix, points.T).T
+
+
+def curvature(contour):
+    x = contour[:, 0]
+    y = contour[:, 1]
+    xp = np.gradient(x)
+    yp = np.gradient(y)
+    xpp = np.gradient(xp)
+    ypp = np.gradient(yp)
+    return (xp * ypp - yp * xpp) / np.power(xp**2 + yp**2, 3 / 2)
+
+
+def zero_crossings(x):
+    return np.where(np.diff(np.sign(x)))[0]
